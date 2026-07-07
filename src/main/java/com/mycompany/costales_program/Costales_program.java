7a31aa87b76c230b088e8cf11a82b1173a6ff5f5
CL2-PC <CL2-PC@CL2-PC27>
2026-07-07 08:55:16 +0800

    ACT-2A FIRST PROGRAM REPOSITORY

---
+++ src/main/java/com/mycompany/costales_program/Costales_program.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.costales_program;
+import java.util.Scanner;
+
+/**
+ *
+ * @author CL2-PC
+ */
+public class Costales_program {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        
+        System.out.println("Hello World!");
+        System.out.println("Costales, Francis Adriane T.");
+        
+        
+        System.out.println("Enter a number:");
+        int x = input.nextInt();
+        
+        System.out.println("Enter a number:");
+        int y = input.nextInt();
+        
+        int sum = x + y;
+        
+        System.out.println("Sum is: " + sum);
+        
+        
+    }
+}
